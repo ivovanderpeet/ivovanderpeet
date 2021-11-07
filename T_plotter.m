f8c232a7ffc9b7adba3d213d907a48d99724dfc6
Ivo van der Peet <i.j.a.van.der.peet@student.tue.nl>
2021-11-07 12:06:18 +0100

    Laatste meting toegevoegd

--- T_plotter.m
+++ T_plotter.m
@@ -1,12 +1,13 @@
 figure(1)
-plot(x,423*ones(size(x))); grid on; hold on;
+plot(x,323*ones(size(x))); grid on; hold on;
 xlim([0, max(x)]);
 xlabel('Length [m]');
 ylabel('Temperature [K]');
 title('Transient behavior of mean wall temperature')
 
 NPLOT = 4;
-tPLOT = [25 50 100];
+tPLOT = [10 20 30 40 50];
+
 
 for i = 1:length(TMID(1,:))
     if max(i*Dt == tPLOT)
@@ -15,7 +16,7 @@ for i = 1:length(TMID(1,:))
         tPLOT = [tPLOT, time];
     end
 end
-text(0.005, 425, 't = 0', 'Color', [0, 0.4470, 0.7410])
-text(0.005, 433, 't = 25', 'Color', [0.8500, 0.3250, 0.0980])
-text(0.005, 438.5, 't = 50', 'Color',[0.9290, 0.6940, 0.1250])
-text(0.04, 445, 't = 100', 'Color',[0.4940, 0.1840, 0.5560])
+% text(0.005, 425, 't = 0', 'Color', [0, 0.4470, 0.7410])
+% text(0.005, 433, 't = 25', 'Color', [0.8500, 0.3250, 0.0980])
+% text(0.005, 438.5, 't = 50', 'Color',[0.9290, 0.6940, 0.1250])
+% text(0.04, 445, 't = 100', 'Color',[0.4940, 0.1840, 0.5560])
